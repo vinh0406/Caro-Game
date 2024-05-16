fcee49c6c8c2fd3f1a4c44258d470f3c50a84eb3
vinh0406 <162810114+vinh0406@users.noreply.github.com>
2024-05-17 01:14:43 +0700

    Update caro-computer.js

--- static/js/caro-computer.js
+++ static/js/caro-computer.js
@@ -118,7 +118,8 @@ function evaluatePosition(row, col, player) {
             const y = col - dy * i;
 
             if (x < 0 || x >= 20 || y < 0 || y >= 20 || board[x * 20 + y].textContent !== player) {
-                count = count - 0.5;
+                if (!Number.isInteger(count)) count = 1;
+                else count = count - 0.5;
                 break;
             }
 
